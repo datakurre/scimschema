703588efd1b1a9bdeeff5240f1e6bcdf0609e451
Asko Soukka <asko.soukka@iki.fi>
2019-05-13 06:16:06 +0300

    Upgrade CI versions

--- setup.nix
+++ setup.nix
@@ -3,8 +3,8 @@
     sha256 = "1j52yvkhw1inp6ilpqy81xv1bbwgwqjn0v9647whampkqgn6dxhk";
   }) {}
 , setup ? import (builtins.fetchTarball {
-    url = "https://github.com/datakurre/setup.nix/archive/24c4a09aa069f03cfac4f19e1ef13f048718cffc.tar.gz";
-    sha256 = "1l8wjibv35wyrc0m8cpcav8ixjdskhdl2piy1pa6fi01z94lwr9n";
+    url = "https://github.com/datakurre/setup.nix/archive/9973f4f4d68776c7a3084e3e7d2635a65b33a35b.tar.gz";
+    sha256 = "176y0d5h0pdf3gv6jz461l4lnz1jcjc5ns19s88yf5vwhib0jb4m";
   })
 , python ? "python3"
 , pythonPackages ? builtins.getAttr (python + "Packages") pkgs
